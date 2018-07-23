974d500cdaaae8e424e36e7e0a28f765d0c05c7d
Adam Gallina <adamg461@gmail.com>
2018-07-23 12:27:16 -0700

    Added a start menu and upgrade/levels menu

---
+++ StickFighterGame/StickFighterGame/Assets/Scripts/_Start Menu/StartMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour {
+    
+	public void StartPressed()
+    {
+        ChangeToScene(1);
+    }
+
+    public void ChangeToScene(int scene)
+    {
+        if (scene == -1)
+        {
+            Application.Quit();
+        }
+        else
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
